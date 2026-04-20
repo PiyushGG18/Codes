09deb0cdb7173caf70204501e3394c7002fd8ed5
PiyushGG18 <gawadepiyush007@gmail.com>
2026-04-20 20:19:23 +0530

    AI-D1

---
+++ AI/02_datatypes/chapter_3.py
@@ -0,0 +1,36 @@
+# Integers
+
+black_tea_grams = 14
+ginger_tea_grams = 3
+
+total_grams = black_tea_grams + ginger_tea_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_tea_grams
+print(f"Total grams of remaining tea is {remaining_tea}")
+
+milk_litres = 7
+servings = 4
+milk_per_serving = milk_litres / servings
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_bags = 7
+pots = 4
+bags_per_pot = total_tea_bags // pots
+print(f"Whole tea bags per pot: {bags_per_pot}")
+
+total_cardamom_pods = 10
+pods_per_cup = 3
+leftover_pods = total_cardamom_pods % pods_per_cup
+
+print(f"Leftover Cardamom pods {leftover_pods}")
+
+base_flavor_strength = 2
+scale_factor = 3
+powerful_flavor = base_flavor_strength ** scale_factor
+print(f"Scaled flavour strength {powerful_flavor}")
+
+total_tea_leaves_harvested = 1_000_000_000
+
+print(f"Tea leaves: {total_tea_leaves_harvested} ")
+
